332c94172e7da98277b83c9d84e28067d2f60dd3
voidrlm <varjun609@gmail.com>
2023-10-21 01:56:14 +0530

    Code changes

--- copy_codebase.sh
+++ copy_codebase.sh
@@ -1,22 +1,27 @@
 destination_directory="E:\dummy"
-echo Copying to destination directory
+echo "Copying to destination directory $destination_directory"
 if [ ! -d "$destination_directory" ]; then
   echo "directory does not exist: $destination_directory"
   sleep 2
 fi
 # add your exclusions hereeee below
 exclude_list=("copy_base.sh" "node_modules" ".git" "dist" ".vscode") 
-sleep 3
+
 for item in ./*; do
-  if [ -d "$item" ] && [[ " ${exclude_list[*]} " == *" $(basename "$item") "* ]]; then
-    continue  # Skip directories in the exclude list
-  elif [[ " ${exclude_list[*]} " == *" $(basename "$item") "* ]]; then
-    continue  # Skip files in the exclude list
-  else
+  item_name=$(basename "$item")
+  exclude=false
+  for excluded_item in "copy_base.sh" "node_modules" ".git" "dist" ".vscode"; do
+    if [ "$item_name" = "$excluded_item" ]; then
+      exclude=true
+      break
+    fi
+  done
+
+  if [ "$exclude" = false ]; then
     cp -r "$item" "$destination_directory"
   fi
 done
-sleep 3
+
 if [ $? -eq 0 ]; then
   echo "Files copied successfully."
 else
