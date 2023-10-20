c684333901d1ad9b7f8c19bbffdb0ca31b215237
voidrlm <varjun609@gmail.com>
2023-10-21 01:17:51 +0530

    Code changes for backing up codebase

--- backup_codebase.sh
+++ backup_codebase.sh
@@ -1,7 +1,6 @@
 
 backup_filename="codeBase_backup_$(date +'%Y%m%d%H%M%S').zip"
-
-zip -r "$backup_filename" .
+find . -type d \( -name .git -o -name node_modules -o -name dist \) -prune -o -type f \( ! -name "backup_codebase.sh" \) -exec zip "$backup_filename" {} +
 echo "Website codebase backup completed successfully."
 echo "Backup file: $backup_dir/$backup_filename"
 sleep 5000
