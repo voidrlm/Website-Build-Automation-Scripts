d2c3b5e4f9f2e86138a031a12e6cd479e698730c
voidrlm <varjun609@gmail.com>
2023-10-21 01:38:16 +0530

    Code changes

--- copy_codebase.sh
+++ copy_codebase.sh
@@ -4,5 +4,9 @@ if [ ! -d "$destination_directory" ]; then
   sleep 3
 fi
 cp -r ./* "$destination_directory"
+if [ $? -eq 0 ]; then
   echo "Files copied successfully."
+else
+  echo "Error copying files."
+  exit 1
 fi
