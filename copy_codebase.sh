a393fb048c91c193ff80496f324c2b347e322f7e
voidrlm <varjun609@gmail.com>
2023-10-21 01:46:46 +0530

    Changes for excluding few files and folders while copying

--- copy_codebase.sh
+++ copy_codebase.sh
@@ -1,9 +1,22 @@
-destination_directory="./dummy/"
+destination_directory="E:\dummy"
+echo Copying to destination directory
 if [ ! -d "$destination_directory" ]; then
   echo "directory does not exist: $destination_directory"
-  sleep 3
+  sleep 2
 fi
-cp -r ./* "$destination_directory"
+# add your exclusions hereeee below
+exclude_list=("copy_base.sh" "node_modules" ".git" "dist" ".vscode") 
+sleep 3
+for item in ./*; do
+  if [ -d "$item" ] && [[ " ${exclude_list[*]} " == *" $(basename "$item") "* ]]; then
+    continue  # Skip directories in the exclude list
+  elif [[ " ${exclude_list[*]} " == *" $(basename "$item") "* ]]; then
+    continue  # Skip files in the exclude list
+  else
+    cp -r "$item" "$destination_directory"
+  fi
+done
+sleep 3
 if [ $? -eq 0 ]; then
   echo "Files copied successfully."
 else
